d9dffd9a7b7a20cafe9e6c21f7d04856803f185c
EXA-Hub <ragabadny4@gmail.com>
2023-12-11 07:36:34 +0200

    subFolders Commit

--- index.js
+++ index.js
@@ -1,57 +1,74 @@
 import 'dotenv/config';
 import TelegramBot from "node-telegram-bot-api";
-import { existsSync, mkdirSync, readdirSync } from "fs";
+import { existsSync, mkdirSync, readdirSync, renameSync } from "fs";
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TOKEN;
-
-const downloadsFolder = "./downloads"; // Replace with your actual downloads folder path
+const downloadsFolder = "./downloads";
 
 if (!existsSync(downloadsFolder)) {
     mkdirSync(downloadsFolder);
 }
 
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('polling_error', (error) => {
     console.error('Polling error:', error);
 });
 
-
 // Matches "/upload [whatever]"
 bot.onText(/\/upload/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, "Enter folder name:").then(() => {
+    let folderPath = downloadsFolder;
+
+    bot.sendMessage(chatId, "Enter first folder name:").then(() => {
         bot.once("message", (msg) => {
-            const folderName = msg.text;
-            const folderPath = `${downloadsFolder}/${folderName}`;
+            const firstFolderName = msg.text;
+            folderPath += `/${firstFolderName}`;
+
             if (!existsSync(folderPath)) {
-                mkdirSync(folderPath);
+                mkdirSync(folderPath, { recursive: true });
             }
-            bot.sendMessage(chatId, "Please upload the file:").then(() => {
-                bot.once("document", (msg) => {
-                    const fileName = msg.document.file_name;
-                    bot.downloadFile(msg.document.file_id, folderPath)
-                        .then(() => {
-                            bot.sendMessage(chatId, `File ${fileName} uploaded successfully!`);
-                        })
-                        .catch((error) => {
-                            console.error("Error downloading file:", error);
-                            bot.sendMessage(chatId, "An error occurred while downloading the file.");
+
+            bot.sendMessage(chatId, "Enter second folder name:").then(() => {
+                bot.once("message", (msg) => {
+                    const secondFolderName = msg.text;
+                    folderPath += `/${secondFolderName}`;
+
+                    if (!existsSync(folderPath)) {
+                        mkdirSync(folderPath, { recursive: true });
+                    }
+
+                    bot.sendMessage(chatId, "Please upload the file:").then(() => {
+                        bot.once("document", (msg) => {
+                            const fileName = msg.document.file_name;
+                            bot.downloadFile(msg.document.file_id, folderPath).then((savedFile) => {
+                                bot.sendMessage(chatId, `File ${fileName} uploaded successfully!`);
+                                const uploadedFilePath = `${folderPath}/${fileName}`;
+                                // Rename the file
+                                renameSync(`.\\${savedFile}`, uploadedFilePath);
+                            }).catch((error) => {
+                                console.error("Error downloading file:", error);
+                                bot.sendMessage(chatId, "An error occurred while downloading the file.");
+                            });
                         });
+                    });
                 });
             });
         });
     });
 });
 
-// Matches "/file [whatever]"
 bot.onText(/\/file/, (msg) => {
     const chatId = msg.chat.id;
-    // Generate buttons for each folder in the downloads directory
-    const folderButtons = readdirSync(downloadsFolder).map(folderName => {
-        return [{ text: folderName, callback_data: `folder_${folderName}` }];
+    const mainFolders = readdirSync(downloadsFolder);
+
+    if (mainFolders.length === 0) {
+        bot.sendMessage(chatId, "No main folders available!");
+        return;
+    }
+
+    const folderButtons = mainFolders.map(folderName => {
+        return [{ text: folderName, callback_data: `mainFolder_${folderName}` }];
     });
 
     const folderOptions = {
@@ -60,65 +77,85 @@ bot.onText(/\/file/, (msg) => {
         })
     };
 
-    bot.sendMessage(chatId, "Choose a folder:", folderOptions).then(foldersChoices => {
-
-        bot.once("callback_query", (callbackQuery) => {
-
-            bot.deleteMessage(chatId, foldersChoices.message_id)
-                .catch(error => {
-                    console.error('Error deleting file message:', error);
-                });
-
-            const folderName = callbackQuery.data.split('_')[1];
-            const folderPath = `${downloadsFolder}/${folderName}`;
-
-            // Check if the folder exists
-            if (!existsSync(folderPath)) {
-                bot.sendMessage(chatId, `Folder ${folderName} does not exist!`);
-                return;
-            }
-
-            // Generate buttons for each file in the folder
-            const fileButtons = readdirSync(folderPath).map(fileName => {
-                return [{ text: fileName, callback_data: `file_${fileName}` }];
+    bot.sendMessage(chatId, "Choose a main folder:", folderOptions).then(mainFoldersChoices => {
+        bot.once("callback_query", (mainFolderCallbackQuery) => {
+            bot.deleteMessage(chatId, mainFoldersChoices.message_id).catch(error => {
+                console.error('Error deleting main folder message:', error);
             });
 
-            if (fileButtons.length === 0) {
-                bot.sendMessage(chatId, `Folder ${folderName} is empty!`);
+            const mainFolderName = mainFolderCallbackQuery.data.split('_')[1];
+            const mainFolderPath = `${downloadsFolder}/${mainFolderName}`;
+
+            if (!existsSync(mainFolderPath)) {
+                bot.sendMessage(chatId, `Main folder ${mainFolderName} does not exist!`);
                 return;
             }
 
-            const fileOptions = {
+            const subFolders = readdirSync(mainFolderPath);
+
+            if (subFolders.length === 0) {
+                bot.sendMessage(chatId, `No subfolders available in ${mainFolderName}!`);
+                return;
+            }
+
+            const subFolderButtons = subFolders.map(subFolderName => {
+                return [{ text: subFolderName, callback_data: `subFolder_${subFolderName}` }];
+            });
+
+            const subFolderOptions = {
                 reply_markup: JSON.stringify({
-                    inline_keyboard: fileButtons
+                    inline_keyboard: subFolderButtons
                 })
             };
 
-            bot.sendMessage(chatId, "Choose a file:", fileOptions).then(filesChoices => {
+            bot.sendMessage(chatId, "Choose a subfolder:", subFolderOptions).then(() => {
+                bot.once("callback_query", (subFolderCallbackQuery) => {
+                    bot.deleteMessage(chatId, subFolderCallbackQuery.message.message_id).catch(error => {
+                        console.error('Error deleting subfolder message:', error);
+                    });
 
-                bot.once("callback_query", (fileCallbackQuery) => {
+                    const subFolderName = subFolderCallbackQuery.data.split('_')[1];
+                    const subFolderPath = `${mainFolderPath}/${subFolderName}`;
 
-                    bot.deleteMessage(chatId, filesChoices.message_id)
-                        .catch(error => {
-                            console.error('Error deleting file message:', error);
+                    const fileButtons = readdirSync(subFolderPath).map(fileName => {
+                        return [{ text: fileName, callback_data: `file_${fileName}` }];
+                    });
+
+                    if (fileButtons.length === 0) {
+                        bot.sendMessage(chatId, `Subfolder ${subFolderName} is empty!`);
+                        return;
+                    }
+
+                    const fileOptions = {
+                        reply_markup: JSON.stringify({
+                            inline_keyboard: fileButtons
+                        })
+                    };
+
+                    bot.sendMessage(chatId, "Choose a file:", fileOptions).then(filesChoices => {
+                        bot.once("callback_query", (fileCallbackQuery) => {
+                            bot.deleteMessage(chatId, filesChoices.message_id).catch(error => {
+                                console.error('Error deleting file message:', error);
+                            });
+
+                            const fileName = (fileCallbackQuery.message.reply_markup.inline_keyboard.find(buttonsFilter =>
+                                buttonsFilter[0].callback_data === fileCallbackQuery.data
+                            ))[0].text;
+                            const filePath = `${subFolderPath}/${fileName}`;
+
+                            bot.sendDocument(chatId, filePath).then(() => {
+                                bot.sendMessage(chatId, "File sent successfully!");
+                            }).catch((err) => {
+                                console.error(err);
+                                bot.sendMessage(chatId, `File ${fileName} does not exist!`);
+                            });
                         });
-
-                    const fileName = (fileCallbackQuery.message.reply_markup.inline_keyboard.find(buttonsFilter =>
-                        buttonsFilter[0].callback_data === fileCallbackQuery.data
-                    ))[0].text;
-                    const filePath = `${folderPath}/${fileName}`;
-
-                    // Send the file
-                    bot.sendDocument(chatId, filePath).then(() => {
-                        bot.sendMessage(chatId, "File sent successfully!");
-                    }).catch((err) => {
-                        console.error(err);
-                        bot.sendMessage(chatId, `File ${fileName} does not exist!`);
                     });
                 });
             });
         });
     });
 });
+
 
 console.log("Bot is running...");
